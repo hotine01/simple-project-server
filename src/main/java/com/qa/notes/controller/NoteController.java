a75cc4191ae8cfbce0b29549b52c4b4888a2ef26
John <hotine@yahoo.com>
2019-11-20 10:33:17 +0000

    fixed code

--- src/main/java/com/qa/notes/controller/NoteController.java
+++ src/main/java/com/qa/notes/controller/NoteController.java
@@ -1,22 +1,22 @@
 package com.qa.notes.controller;
 
 
-//import java.util.ArrayList;
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseStatus;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-//import com.qa.notes.dto.NoteDto;
+import com.qa.notes.dto.NoteDto;
 import com.qa.notes.service.NoteService;
 
 /**
@@ -24,8 +24,8 @@ import com.qa.notes.service.NoteService;
  *
  */
 
-//@RestController
-//@CrossOrigin("*")
+@RestController
+@CrossOrigin("*")
 public class NoteController {
 	
 	@Autowired
